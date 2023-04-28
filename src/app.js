5a446422b948381f02904f28e3a2f195c1c24cae
MarkoErdelji <erdeljimarko@gmail.com>
2023-04-28 19:59:30 +0200

    feat: Added register and login with jwt (users saved to dynamoDB) and file upload to S3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const auth = require('./routes/auth');
+const uploadRouter = require('./routes/upload');
+const dotenv = require('dotenv');
+const path = require('path');
+
+const envPath = path.resolve(__dirname, './config.env');
+dotenv.config({ path: envPath });
+
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+
+
+app.use(express.json());
+
+app.use('/auth', auth.router);
+app.use('/upload', auth.verifyAuth,uploadRouter);
+
+
 module.exports = app;
